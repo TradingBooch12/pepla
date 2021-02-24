debb011598b17b7ed1e75c6fd0a1e9582ba6b25a
TradingBooch12 <71621609+TradingBooch12@users.noreply.github.com>
2021-02-23 14:42:59 -1000

    v1.0.1

--- commands/info.js
+++ commands/info.js
@@ -9,9 +9,9 @@ module.exports = {
 
         info.setTitle("Pepla Bot Information");
         info.setDescription("I am a bot that was sent by Peppa");
-        info.addField("Version:", "v1.0.0", false);
+        info.addField("Version:", "v1.0.1", false);
         info.addField("Named by:", "<@731583599400190012>", false);
-        info.setAuthor("Created by TradingBooch12 and Meet_TheE1i");
+        info.setAuthor("Created by <@627426891288018944> and <@532009975359471616>");
         info.setColor(0xff50d1);
         
         message.channel.send(info);
